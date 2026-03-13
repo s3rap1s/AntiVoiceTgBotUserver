9494dafaad0a8b83f177fa146ebd3d43416a151e
s3rap1s <sup.mar001@gmail.com>
2026-03-13 03:49:07 +0300

    refactor: separate telegram_api as a standalone library https://github.com/s3rap1s/telegram-api-userver

--- telegram_api/src/types/message_ptr.cpp
+++
@@ -1,8 +0,0 @@
-#include <tg/types/message.hpp>
-#include <tg/types/message_ptr.hpp>
-
-namespace tg {
-
-void MessageDeleter::operator()(Message* p) const noexcept { delete p; }
-
-}  // namespace tg
