fe5d33fa75402dd4a60b2d1e36f48bd487e1dbbf
s3rap1s <sup.mar001@gmail.com>
2026-03-21 19:44:10 +0300

    refactor: small speed description changes

--- src/bot/config.hpp
+++ src/bot/config.hpp
@@ -11,7 +11,7 @@ inline constexpr size_t MAX_QUERY_SIZE = 256;
 inline constexpr size_t MAX_MESSAGE_SIZE = 4096;
 inline constexpr size_t DEFAULT_SPEED_INDEX = 0;
 
-const std::vector<SpeedInformation> SPEEDS = {{5.0, 1, "🐌 Very Slow (Torture Mode)", "5s/word"},
+const std::vector<SpeedInformation> SPEEDS = {{5.0, 1, "🐌 Very Slow", "5s/word"},
                                               {3.0, 2, "🐢 Slow", "3s/2words"},
                                               {1.5, 3, "🦮 Medium", "1.5s/3words"},
                                               {1.0, 5, "🐆 Fast", "1s/5words"}};
